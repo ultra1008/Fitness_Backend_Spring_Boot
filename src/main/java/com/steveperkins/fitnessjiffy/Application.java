0a93732e23bc79dfe0083f604c6d5fac1cff2188
ultra1008 <ultra921008@gmail.com>
2019-11-10 17:30:15 -0500

    First steps toward integrating JWT with the existing Spring Security process

--- src/main/java/com/steveperkins/fitnessjiffy/Application.java
+++ src/main/java/com/steveperkins/fitnessjiffy/Application.java
@@ -2,13 +2,12 @@ package com.steveperkins.fitnessjiffy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(final String[] args) {
-        final ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/steveperkins/fitnessjiffy/config/SecurityConfig.java
+++ src/main/java/com/steveperkins/fitnessjiffy/config/SecurityConfig.java
@@ -6,22 +6,26 @@ import com.steveperkins.fitnessjiffy.dto.converter.UserToUserDTO;
 import com.steveperkins.fitnessjiffy.repository.UserRepository;
 import com.steveperkins.fitnessjiffy.service.ReportDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.Collection;
 import java.util.HashSet;
@@ -30,14 +34,105 @@ import java.util.HashSet;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    ReportDataService reportDataService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private UserToUserDTO userDTOConverter;
+
+    /**
+     * Sets up Spring Security rules for which URL patterns to exclude from authentication checks, as well as
+     * config for the login form and logout link.
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/favicon.ico").permitAll()
+                .antMatchers("/static/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .successHandler(new AuthSuccessHandler())
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    /**
+     * Spring Security framework-level method, to set the object that will verify a username and password during
+     * the login process.  See {@link this#buildDaoAuthenticationProvider()}.
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(buildDaoAuthenticationProvider());
+    }
+
+    /**
+     * Factory method for the object that verifies a username and password during the login process.
+     *
+     * @return
+     */
+    @Nonnull
+    private DaoAuthenticationProvider buildDaoAuthenticationProvider() {
+        final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(username -> {
+            final User user = userRepository.findByEmailEquals(username);
+            final UserDTO userDTO = userDTOConverter.convert(user);
+            return (user == null || userDTO == null) ? null : new SpringUserDetails(userDTO, user.getPasswordHash());
+        });
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    /**
+     * The {@link this#configure(HttpSecurity)} method registers an instance of this class, to be executed upon
+     * every successful login.  This logic updates the user's last-login-date, kicks off an update of the user's
+     * report data, and stores a JWT token as a browser cookie.
+     *
+     * TODO: Need a way to make Spring Security check for the JWT token in a cookie (and/or header), and allow or block based on that
+     */
+    private final class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            // Retrieve the user
+            final SpringUserDetails userDetails = (SpringUserDetails) authentication.getPrincipal();
+            final UserDTO userDTO = userDetails.getUserDTO();
+            final User user = userRepository.findOne(userDTO.getId());
+
+            // Schedule a ReportData update
+            final Date lastUpdateDate = new Date(user.getLastUpdatedTime().getTime());
+            reportDataService.updateUserFromDate(user, lastUpdateDate);
+
+            // TODO: Create a JWT token and store it in a cookie
+            response.addCookie(new Cookie("foo", "bar"));
+            super.onAuthenticationSuccess(request, response, authentication);
+        }
+    }
+
+    /**
+     * A custom override of the Spring Security level POJO for authenticated users.  After authentication, Spring
+     * Security makes this available through {@link SecurityContextHolder#getContext()#getAuthentication()#getPrincipal()}.
+     */
     public static final class SpringUserDetails implements UserDetails {
 
         private final UserDTO userDTO;
         private final String password;
 
         public SpringUserDetails(
-            @Nonnull final UserDTO userDTO,
-            @Nonnull final String password
+                @Nonnull final UserDTO userDTO,
+                @Nonnull final String password
         ) {
             this.userDTO = userDTO;
             this.password = password;
@@ -85,70 +180,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         public boolean isEnabled() {
             return true;
         }
-    }
-
-    public final class LoginListener implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
-
-        @Override
-        public void onApplicationEvent(final InteractiveAuthenticationSuccessEvent event) {
-            // Retrieve the user
-            final SpringUserDetails userDetails = (SpringUserDetails) event.getAuthentication().getPrincipal();
-            final UserDTO userDTO = userDetails.getUserDTO();
-            final User user = userRepository.findOne(userDTO.getId());
-
-            // Schedule a ReportData update
-            final Date lastUpdateDate = new Date(user.getLastUpdatedTime().getTime());
-            reportDataService.updateUserFromDate(user, lastUpdateDate);
-        }
-
-    }
-
-    @Autowired
-    ReportDataService reportDataService;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    private UserToUserDTO userDTOConverter;
-
-    /** This bean must be manually declared, since Spring's autoscanning apparently won't find a @Component-annotated inner class. */
-    @Bean
-    public LoginListener loginListener() {
-        return new LoginListener();
-    }
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/favicon.ico").permitAll()
-                .antMatchers("/static/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(buildDaoAuthenticationProvider());
-    }
-
-    @Nonnull
-    private DaoAuthenticationProvider buildDaoAuthenticationProvider() {
-        final DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(username -> {
-            final User user = userRepository.findByEmailEquals(username);
-            final UserDTO userDTO = userDTOConverter.convert(user);
-            return (user == null || userDTO == null) ? null : new SpringUserDetails(userDTO, user.getPasswordHash());
-        });
-        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
-        return daoAuthenticationProvider;
     }
 
 }
